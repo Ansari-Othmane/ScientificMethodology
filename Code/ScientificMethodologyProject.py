df9beca92f7b6750062615dd7c1bf4e1556c5193
Ansari-Othmane <otmane.ansari@usmba.ac.ma>
2017-12-17 22:36:28 +0100

    dropOutliers function added plus correction
    -dropOutliers was added
    -gainPercentage was corrected (inverse previous value with current one)

--- Code/ScientificMethodologyProject.py
+++ Code/ScientificMethodologyProject.py
@@ -20,10 +20,32 @@ def gainPercentage(ts):
     data = ts[1:]
     previous_index = ts.index[0]
     for index_ts, row_ts in data.iteritems():
-        gain_value = (ts[previous_index]-ts[index_ts])/ts[previous_index]
+        gain_value = (ts[index_ts]-ts[previous_index])/ts[previous_index]
         ts_gain.loc[index_ts] = gain_value
         previous_index = index_ts
     return ts_gain.gain
+
+
+
+
+def dropOutliers(train_data,sd_coef):
+    data_gain = gainPercentage(train_data)
+    mean_gain = np.mean(data_gain.values)
+    risk = np.std(data_gain.values)
+    
+    for gain,index in zip(data_gain.values,data_gain.index):
+        if gain > mean_gain + (risk*sd_coef) or gain < mean_gain - (risk*sd_coef):
+            if data_gain[0] == gain:
+                data_gain[0] = data_gain[1]
+            else :
+                if data_gain[-1] == gain:
+                    data_gain[-1] = data_gain[-2]
+                else:
+                    prev_val = data_gain.shift(1)[data_gain.index.get_loc(index)]
+                    next_val = data_gain.shift(-1)[data_gain.index.get_loc(index)]
+                    data_gain[data_gain.index.get_loc(index)] = (prev_val + next_val ) / 2
+        
+    return data_gain
     
     
     
