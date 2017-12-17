637228b235e2e758bba8455763afca455b48aab5
Ansari-Othmane <otmane.ansari@usmba.ac.ma>
2017-12-17 18:54:46 +0100

    Update ScientificMethodologyProject.py

--- Code/ScientificMethodologyProject.py
+++ Code/ScientificMethodologyProject.py
@@ -1,1 +1,18 @@
+from pandas_datareader import data
+%matplotlib inline  
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+def plotData(data,x_label,y_label,title):
+    plt.rcParams["figure.figsize"] = [15.0,10.0]
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.legend('price legend')
+    data.plot()
+    
+    
+    
 
