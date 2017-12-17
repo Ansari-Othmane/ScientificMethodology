2327c1129109ce4fcfef1d979003d6fc598760b5
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2017-12-17 21:18:51 +0100

    Add 'gainPercentage()' function

--- Code/ScientificMethodologyProject.py
+++ Code/ScientificMethodologyProject.py
@@ -2,6 +2,7 @@ from pandas_datareader import data
 %matplotlib inline  
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
 
@@ -12,6 +13,17 @@ def plotData(data,x_label,y_label,title):
     plt.title(title)
     plt.legend('price legend')
     data.plot()
+    
+    
+def gainPercentage(ts):
+    ts_gain = pd.DataFrame(columns=['gain'])
+    data = ts[1:]
+    previous_index = ts.index[0]
+    for index_ts, row_ts in data.iteritems():
+        gain_value = (ts[previous_index]-ts[index_ts])/ts[previous_index]
+        ts_gain.loc[index_ts] = gain_value
+        previous_index = index_ts
+    return ts_gain.gain
     
     
     
