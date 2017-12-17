65d84f6547cc2edcebd8755698c81911047a4f37
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2017-12-17 23:39:11 +0100

    Add getData() function

--- Code/ScientificMethodologyProject.py
+++ Code/ScientificMethodologyProject.py
@@ -1,10 +1,31 @@
+from datetime import timedelta
+from datetime import datetime
 from pandas_datareader import data
 %matplotlib inline  
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-
+def getData(stock_name, start_date, end_date, event_date, source='yahoo'):
+    # Convert date parameters into datetime type
+    start = datetime.strptime(start_date, '%Y-%m-%d')
+    end = datetime.strptime(end_date, '%Y-%m-%d')
+    event = datetime.strptime(event_date, '%Y-%m-%d')
+    
+    # Get data using an API
+    ts = data.DataReader(stock_name, source, start, end)['Adj Close']
+    
+    '''
+    Get the number of days we have in our for our training+validation, 
+    then have the training be on 80% of these, the validation on 20%
+    '''
+    train_days = (event - start).days
+    inter_date = start+timedelta(days=int(train_days*0.8))
+    ts_train = ts[start:inter_date]
+    ts_validate = ts[inter_date+timedelta(days=1):event-timedelta(days=1)]
+    ts_test = ts[event:]
+    
+    return ts_train, ts_validate, ts_test
 
 def plotData(data,x_label,y_label,title):
     plt.rcParams["figure.figsize"] = [15.0,10.0]
@@ -17,16 +38,23 @@ def plotData(data,x_label,y_label,title):
     
 def gainPercentage(ts):
     ts_gain = pd.DataFrame(columns=['gain'])
+    
+    # Get the data starting from the 2nd element
     data = ts[1:]
+    
+    '''
+    The formula of the gain being the current stock price minus the previous one, devided by the latter, 
+    we'll need to keep track of the previous index at all times
+    '''
     previous_index = ts.index[0]
+    
+    # Iterating over the data without the  first element, and compute the gain using the aforementioned formula
     for index_ts, row_ts in data.iteritems():
         gain_value = (ts[index_ts]-ts[previous_index])/ts[previous_index]
         ts_gain.loc[index_ts] = gain_value
         previous_index = index_ts
+    
     return ts_gain.gain
-
-
-
 
 def dropOutliers(train_data,sd_coef):
     data_gain = gainPercentage(train_data)
